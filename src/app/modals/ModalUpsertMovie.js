f57c2a2363a03f35660c372a236c76ab6eae6222
jersonvictor <jersonvictor24@gmail.com>
2020-02-26 21:46:12 -0500

    update(pages:dashboard):validation form upsert movie

--- src/app/modals/ModalUpsertMovie.js
+++ src/app/modals/ModalUpsertMovie.js
@@ -14,14 +14,19 @@ const ModalUpsertMovie = (props) => {
 
     const [loader, setLoader] = useState(false);
 
-    const { register, handleSubmit } = useForm({
+    const { register, handleSubmit, errors } = useForm({
         mode: 'onChange'
     });
+
+    // cancel if re-render
+    if (!isOpen && !movie) return null;
+
+
 
     const title = movie ? 'Actualizar Movie' : 'Registrar Movie';
     const mode = movie ? 'update' : 'create';
 
-    if (!isOpen && !movie) return null;
+    // can you use uffect for get detail the movie
 
     const handleSubmitForm = (valueForm) => {
         //  you can  clean and | or format  with function
@@ -37,41 +42,53 @@ const ModalUpsertMovie = (props) => {
             .finally(() => setLoader(false))
     };
 
+    const ErrorRequired = () => (<div className='text-right'><span className='text-xs text-red-600'>Campo requerido</span> </div>);
+
     return (
         <JModalLayout title={title} isOpen={isOpen} footer={false} onClose={onClose}>
             <form onSubmit={handleSubmit(handleSubmitForm)}>
                 <div className='flex mb-2'>
                     <label htmlFor="name" className='w-64 flex-shrink-0'>Nombre de Película</label>
-                    <input
-                        type="text"
-                        name='name'
-                        defaultValue={(movie && movie.name) || ''}
-                        className='flex-grow border rounded-sm px-2 py-1 text-sm'
-                        ref={register({ required: true })}
-                    />
+                    <div className='flex-grow'>
+                        {errors.name && <ErrorRequired />}
+                        <input
+                            type="text"
+                            name='name'
+                            defaultValue={(movie && movie.name) || ''}
+                            className='block w-full border rounded-sm px-2 py-1 text-sm'
+                            ref={register({ required: true })}
+                        />
+                    </div>
+
                 </div>
                 <div className='flex mb-2'>
                     <label htmlFor="name" className='w-64 flex-shrink-0'>Fecha de Publicación</label>
-                    <input
-                        type="date"
-                        name='pub_date'
-                        defaultValue={(movie && movie.pub_date) || ''}
-                        className='flex-grow border rounded-sm px-2 py-1 text-sm'
-                        ref={register({ required: true })}
-                    />
+                    <div className='flex-grow'>
+                        {errors.pub_date && <ErrorRequired />}
+                        <input
+                            type="date"
+                            name='pub_date'
+                            defaultValue={(movie && movie.pub_date) || ''}
+                            className='block w-full flex-grow border rounded-sm px-2 py-1 text-sm'
+                            ref={register({ required: true })}
+                        />
+                    </div>
                 </div>
                 <div className='flex'>
                     <label htmlFor="name" className='w-64 flex-shrink-0'>Estado</label>
-                    <select
-                        name="state"
-                        ref={register({ required: true })}
-                        className='flex-grow border rounded-sm px-2 py-1 text-sm'
-                        defaultValue={(movie && movie.state) || ''}
-                    >
-                        <option value="" disabled>Seleccione Estado</option>
-                        <option value="active">Activo</option>
-                        <option value="inactive">Inactivo</option>
-                    </select>
+                    <div className='flex-grow'>
+                        {errors.state && <ErrorRequired />}
+                        <select
+                            name="state"
+                            ref={register({ required: true })}
+                            className='block w-full border rounded-sm px-2 py-1 text-sm'
+                            defaultValue={(movie && movie.state) || ''}
+                        >
+                            <option value="" disabled>Seleccione Estado</option>
+                            <option value="active">Activo</option>
+                            <option value="inactive">Inactivo</option>
+                        </select>
+                    </div>
                 </div>
                 <div className='text-right mt-6'>
                     <button type='submit' disabled={loader} className='px-3 py-1 bg-blue-600 rounded-sm text-white'>
